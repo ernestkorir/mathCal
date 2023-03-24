4a80bcdb578d8e219f6d8ca37c8ba6cdca49dd80
Ernest Korir <ernestbore3@gmail.com>
2023-03-24 23:12:38 +0300

    Use load and error state

--- src/components/Quotes.js
+++ src/components/Quotes.js
@@ -1,51 +1,66 @@
-import './Quotes.css';
-import { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { FaCircleNotch, FaQuoteLeft } from 'react-icons/fa';
 
 function Quotes() {
-  const [quote, setQuote] = useState({});
+  const [data, setData] = useState([]);
+  const [hasError, setHasError] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const API_KEY = 'zWbFdXe3BQWrlhFsnImSAw==HEDkeSdVW05kaUH7';
+  const apiKey = 'zWbFdXe3BQWrlhFsnImSAw==HEDkeSdVW05kaUH7';
+  const category = 'knowledge';
+
   useEffect(() => {
-    fetch('https://api.api-ninjas.com/v1/quotes?category=knowledge', {
-      headers: { 'X-Api-Key': API_KEY },
-      method: 'GET',
-      contentType: 'application/json',
-    }).then((response) => response.json()).then((result) => {
-      if (result.error) {
-        setQuote({ quote: 'An Error Occurred' });
-      } else {
-        setQuote(result[0]);
+    const fetchQuote = async () => {
+      setIsLoading(true);
+      try {
+        const res = await fetch(`https://api.api-ninjas.com/v1/quotes?category=${category}`, {
+          method: 'GET',
+          headers: {
+            'X-Api-Key': apiKey,
+            'Content-Type': 'application/json',
+          },
+        });
+        const json = await res.json();
+        setData(json);
+      } catch (error) {
+        setHasError(true);
       }
-    });
-  }, []);
+      setIsLoading(false);
+    };
+    fetchQuote();
+  }, [setData, setIsLoading]);
+
+  if (hasError) {
+    return (
+      <div>
+        <p className="error">Something went wrong!</p>
+      </div>
+    );
+  }
+  if (isLoading) {
+    return (
+      <div>
+        <div className="spinner"><FaCircleNotch size={70} /></div>
+      </div>
+    );
+  }
 
   return (
-   // eslint-disable-next-line
-    !quote.quote
-      ? (
-        <div className="quotes">
-          <div className="spinner-box">
-            <div className="configure-border-1">
-              <div className="configure-core" />
-            </div>
-            <div className="configure-border-2">
-              <div className="configure-core" />
-            </div>
-          </div>
-        </div>
-      )
-      : (quote.author
-        ? (
-          <div className="quotes">
-            <p className="quotes__quote">{quote.quote}</p>
-            <p className="quotes__author">
-              &quot;
-              {quote.author}
-              &quot;
+    <section>
+      <div id="quote-box">
+        {data.map((item) => (
+          <blockquote key={data.indexOf(item)} className="blockquote">
+            <p className="quote">
+              <FaQuoteLeft size={42} />
+              <span id="text">{item.quote}</span>
             </p>
-          </div>
-        ) : (<div className="quotes"><p>{quote.quote}</p></div>)
-      )
+            <footer className="blockquote-footer" id="author">
+              {item.author}
+            </footer>
+          </blockquote>
+        ))}
+      </div>
+    </section>
   );
 }
 
